d0607e12ac8fb67b45496b32f6a53777ffebfdf3
harryxu <harryzhxu@gmail.com>
2010-07-14 17:13:32 +0800

    init

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'django-pass',
+    version = '0.1',
+    packages = find_packages(),
+
+    author = 'harryxu',
+    author_email = 'harryzhxu@gmail.com',
+    license = 'BSD',
+    description = 'Django user account utils',
+    url = 'http://github.com/harryxu/django-pass',
+)
